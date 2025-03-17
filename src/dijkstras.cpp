474e5e4e89df6e72a26d6b11da5f202c8c38d3a9
NisuJT <tranjustin18@gmail.com>
2025-03-17 02:01:12 +0000

    fixed bugs

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -4,22 +4,21 @@ struct compareWeight{
     bool operator()(const pair<int,int>& a, const pair<int,int>& b){
         return a.second > b.second;
     }
-}
+};
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous){
     int numVertices = G.size();
     vector<int> distances(numVertices, INF);
-    vector<bool> visited(num , false);
+    vector<bool> visited(numVertices , false);
     distances[source] = 0;
     previous[source] = -1;
-    priority_queue<pair<int,int>, compareWeight> minHeap; 
+    priority_queue<pair<int,int>, vector<pair<int, int>>, compareWeight> minHeap; 
     minHeap.push({source, 0});
     while (!minHeap.empty()){
-        auto current = minHeap.top();
+        int u = minHeap.top().first;
         minHeap.pop();
-        u = current.first;
         if(visited[u]) continue;
         visited[u] = true;
-        for(Edge edge : graph[u]) {
+        for(Edge edge : G[u]) {
             int v = edge.dst;
             int weight = edge.weight;
             if(!visited[v] && distances[u] + weight < distances[v]){
@@ -34,11 +33,12 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination){
     vector<int> shortest_path;
     if(distances[destination] == INF)
-        return path;
+        return shortest_path;
     for(int i = destination; i != -1; i = previous[i])
         shortest_path.push_back(i);
 
-    return reverse(shortest_path.begin(), shortest_path.end())
+    reverse(shortest_path.begin(), shortest_path.end());
+    return shortest_path;
 }
 
 void print_path(const vector<int>& v, int total){
